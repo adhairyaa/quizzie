e35eaedcbae811fb7db94b0ad593e0018a1b723a
Dhairya <dhairyagulati370@gmail.com>
2023-02-01 15:22:52 +0530

    Add : Icons

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,14 +3,25 @@ import { Link } from "react-router-dom";
 import { useTheme } from "../../context/themeContext/ThemeProvider";
 import "./Navbar.css";
 export function Navbar() {
-  const { theme, toggleTheme } = useTheme();
+  const { toggleTheme } = useTheme();
   return (
     <div className="navbar">
       <Link to="/">
-        <div>Home</div>
+        <div className="home-icon">
+          <img
+            src="https://img.icons8.com/neon/256/home-page.png"
+            alt="icon"
+          ></img>
+        </div>
       </Link>
-      <button onClick={toggleTheme as MouseEventHandler<HTMLButtonElement>}>
-        {theme}
+      <button
+        onClick={toggleTheme as MouseEventHandler<HTMLButtonElement>}
+        className="themeButton"
+      >
+        <img
+          src="https://img.icons8.com/ios/256/ceiling-light.png"
+          alt="light"
+        ></img>
       </button>
     </div>
   );
