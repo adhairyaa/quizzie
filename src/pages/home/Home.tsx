f0a8700f5641217b286a81c71aee2af9f9dac0f6
Dhairya <dhairyagulati370@gmail.com>
2023-02-01 15:11:19 +0530

    minor Css fix

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,9 +5,16 @@ function Home() {
   return (
     <>
       <div className="quizes-container">
-        {allQuiz.quizes.map((quiz, index) => (
-          <QuizCard coverImage={quiz.image} title={quiz.name} index={index} />
-        ))}
+        <div className="quiz-heading">
+          <span> quizzie </span>
+          Are you a comic enthusiast? How about we ask a few question and check
+          your love for them ♥.
+        </div>
+        <div className="quiz-types">
+          {allQuiz.quizes.map((quiz, index) => (
+            <QuizCard coverImage={quiz.image} title={quiz.name} index={index} />
+          ))}
+        </div>
       </div>
     </>
   );
